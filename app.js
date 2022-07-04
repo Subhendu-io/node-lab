2f15b839776e00d41422a867191764f8749f1dff
Subhendu-io <subhendu.luv@gmail.com>
2022-07-04 16:16:31 +0530

    1 file modified

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
-const realPin = '5555';
-
+const realPin = '8888';
+let count = 0;
 tryPin = (pin) => {
-  const total = [...realPin]
-    .map((dig, i) => [...pin][i] - dig)
-    .reduce((a, b) => Math.abs(a) + Math.abs(b), 0);
+  count++;
+  let total = 0;
+  for (let i = 0; i < 4; i++) {
+    total += Math.abs(parseInt(pin.charAt(i)) - parseInt(realPin.charAt(i)));
+  }
+
   return total;
 };
 
-console.log(tryPin('6644'));
+let guss, diff, add;
+
+if (tryPin('0000') > tryPin('9999')) {
+  add = -1;
+  guss = '9999';
+  diff = tryPin('9999');
+} else {
+  add = 1;
+  guss = '0000';
+  diff = tryPin('0000');
+}
+
+let i = 0;
+
+hack = () => {
+  if (diff && i < 3) {
+    let newGuss =
+      guss.substring(0, i) +
+      (parseInt(guss.charAt(i)) + add) +
+      guss.substring(i + 1);
+    if (tryPin(newGuss) > diff) {
+      i++;
+    } else {
+      guss = newGuss;
+      diff--;
+    }
+    hack();
+  } else {
+    guss = guss.substring(0, i) + (parseInt(guss.charAt(i)) + diff);
+    console.log(guss, count);
+  }
+};
+
+hack();
